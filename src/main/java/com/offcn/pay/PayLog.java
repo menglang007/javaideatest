2edfc2b8976231744f877fe596c329bedd39d6eb
java-test <hkm6364@126.com>
2019-06-28 22:12:50 +0800

    开发微信支付

---
+++ src/main/java/com/offcn/pay/PayLog.java
@@ -0,0 +1,10 @@
+package com.offcn.pay;
+
+public class PayLog {
+
+    public void log(){
+        System.out.println("支付日志！！！");
+    }
+
+
+}
---
+++ src/main/java/com/offcn/pay/WenXinPay.java
@@ -0,0 +1,20 @@
+package com.offcn.pay;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WenXinPay {
+
+
+    public String pay(String out_trade_no){
+        System.out.println("wei信支付");
+        return "success";
+    }
+
+    public Map query(String out_trade_no){
+        Map map =new HashMap();
+        return map;
+    }
+
+
+}
